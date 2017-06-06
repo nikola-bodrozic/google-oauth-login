f4ad57f8be618621dc884ea37cdfb6ab7cccfd3b
Nikola Bodrozic <nikola.bodrozic@users.noreply.github.com>
2017-06-06 10:32:22 +0200

    added comments

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login with google</title>
+  </head>
+  <body>
+        <title>Login with Google Account</title>
 <?php
 
+// autoload classes
 require_once __DIR__.'/vendor/autoload.php';
 
+// get OAuth library
 require 'oauth-params.inc';
 
 session_start();
 
+// create instance of Google_Client class
 $client = new Google_Client();
 $client->setClientId(CLIENT_ID);
 $client->setClientSecret(CLIENT_SECRET);
@@ -14,17 +27,20 @@ $client->setScopes('email');
 
 $plus = new Google_Service_Plus($client);
 
+// logout user
 if (isset($_REQUEST['logout'])) {
    session_unset();
 }
 
+// redirect user to secure page on our site after login
 if (isset($_GET['code'])) {
   $client->authenticate($_GET['code']);
   $_SESSION['access_token'] = $client->getAccessToken();
-  $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+  $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
   header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
 }
 
+// get data about user if all is ok with access token
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
   $me = $plus->people->get('me');
@@ -57,11 +73,12 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
         print "ID: {$id} <br>";
         print "Name: {$name} <br>";
         print "Email: {$email } <br>";
-        print "Image : {$profile_image_url} <br>";
+        print "<img src='{$profile_image_url}'> <br>";
         print "Cover  :{$cover_image_url} <br>";
         print "Url: {$profile_url} <br><br>";
         echo "<a class='logout' href='?logout'><button>Logout</button></a>";
     }
     ?>
 </div>
-
+</body>
+</html>
